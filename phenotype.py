7910c69714819dd0043e4008091b2568bd480ddb
Julien Deswaef <juego@requiem4tv.com>
2016-10-26 13:41:42 -0400

    Changed from printing results in the console to filling up a list.

--- phenotype.py
+++ phenotype.py
@@ -9,53 +9,54 @@ from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
 
 class Phenotype:
-    
+
     traits = dict() #actual traits of this genotype
-    
+
     possibleTraitsList = [] #various possible traits
     possibleTraits = dict()
 
 
-        
+
     def loadPossibleSNPs(self, filename):
-        #load a CSV of SNPs -> traits 
-        print "reading  data from " + filename 
+        #load a CSV of SNPs -> traits
+        print "reading  data from " + filename
         reader = csv.reader(open(filename, "rU"), delimiter=',')
         for row in reader:
             #print row
             if (len(row) >= 8):
                 if row[0] == '':
                     continue
-                
+
                 if self.possibleTraits.has_key(row[0]):
                     trait = self.possibleTraits[row[0]]
                 else:
                     trait = Trait(row[0], row[-1])
-                    
+
                 i=1
                 while i< (len(row)-1):
-                    trait.addAllele(row[i], row[i+1]) 
+                    trait.addAllele(row[i], row[i+1])
                     i=i+1
-                
+
                 self.possibleTraits[trait.rsid] = trait #add this new trait to dict indexed by RSID
                 self.possibleTraitsList.append(trait) #add to list of traits as well
                 #print trait
             else:
                 print "row of incorrect length" , row
-                
+
         print "done reading possible SNPs\n"
-        
+
     def mapGenoToPheno(self, genotype):
         #for each possible trait see if we have a genotype
         #if we do add it with its value to the traits dict
+        results = []
         for trait in self.possibleTraitsList:
             if genotype.has_key(trait.rsid):
                 self.traits[trait.rsid] = genotype[trait.rsid]
                 if trait.alleles.has_key(genotype[trait.rsid]):
-                    print trait.rsid, " - ", genotype[trait.rsid], " - ", trait.alleles[genotype[trait.rsid]]
+                    results.append( trait.rsid + " - " + genotype[trait.rsid] + " - " + trait.alleles[genotype[trait.rsid]] )
                 # if not try flipping the order of the alleles
                 elif trait.alleles.has_key(genotype[trait.rsid][::-1]):
-                     print trait.rsid, " - ", genotype[trait.rsid][::-1], " (flipped) - ", trait.alleles[genotype[trait.rsid][::-1]]
+                     results.append( trait.rsid + " - " + genotype[trait.rsid][::-1] + " (flipped) - " + trait.alleles[genotype[trait.rsid][::-1]] )
                 else:
                     #try reverse complement
                     #print genotype[trait.rsid]
@@ -63,13 +64,14 @@ class Phenotype:
                     rev = str(my_dna.complement())
                     #print rev
                     if trait.alleles.has_key(rev):
-                        print trait.rsid, " - ", rev, " (rev comp) -", trait.alleles[rev]
+                        results.append( trait.rsid + " - " + rev + " (rev comp) -" + trait.alleles[rev] )
                     # if not try flipping the order of the alleles
                     elif trait.alleles.has_key(rev[::-1]):
-                        print trait.rsid, " - ", rev[::-1], " (flipped) - ", trait.alleles[rev[::-1]]
+                        results.append( trait.rsid + " - " + rev[::-1] + " (flipped) - " + trait.alleles[rev[::-1]] )
                     else:
-                        print "genotype " , genotype[trait.rsid], "and rev comp " , rev, " not found in traits for " , trait.rsid   
-                        
+                        results.append( "genotype " + genotype[trait.rsid] + "and rev comp " + rev + " not found in traits for " + trait.rsid )
+
             else:
                # print trait, " genotype not found for ", trait.rsid, " available genotype mappings: ", trait.alleles
-                print "genotype not found for ", trait.rsid
+                results.append( "genotype not found for " + trait.rsid )
+        return results
